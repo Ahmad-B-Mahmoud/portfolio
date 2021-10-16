1c8117b3578f67eac3244df71588f451580a2efb
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2021-10-17 01:59:59 +0300

    Qery Preps CMS Projects

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,58 @@
 import Head from 'next/head'
+import Header from '../components/layouts/Header'
+import {prepr} from '../lib/prepr'
 
-export default function Home() {
+export default function Home({ projects }) {
   return (
     <div>
       <Head>
         <title>Ahmad Mahmoud</title>
-        <meta name="description" content="My Personal Portfolio" />
+        <meta name="description" content="My Personal Portfolio." />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      <Header />
     </div>
   )
 }
+
+export async function getStaticProps() {
+  
+  // Query publications
+  const getprojects = await prepr
+    .graphqlQuery(`{
+      Projects(limit : 10 ) {
+        items {
+          _id,
+          title,
+          description,
+          thumbnail {
+            url,
+            height,
+            width
+          },
+          site_url,
+          github_ripo,
+          site_icon {
+            url,
+            height,
+            width
+          },
+          build_with {
+            url,
+            height,
+            width
+          }
+        }
+      }
+      }`)
+    .fetch();
+
+  // Pass publications as props
+  return {
+    props: {
+      projects: getprojects.data.Projects.items
+    },
+  };
+}
+
