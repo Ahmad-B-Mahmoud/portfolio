d04d16f8cc0924aeab8854e78c5cc93b4ac99777
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2021-10-17 02:22:29 +0300

    Add General Theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffc857ff',
+    },
+    secondary: {
+      main: '#daffedff',
+    },
+    dark: {
+      main: '#1f2041ff'
+    },
+    success: {
+      main: '#19647eff'
+    }
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
+
