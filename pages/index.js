acf3f70efcc3284369c4557b4e2f39207647d5c5
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2021-10-26 16:34:47 +0300

    Add Site favicons

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,13 @@ export default function Home({ projects }) {
         <meta name="description" content="My Personal Portfolio." />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#4b3f72" />
+        <meta name="theme-color" content="#4b3f72"></meta>
       </Head>
       <Script
         id="moment"
