4b2947679296bbfe2ae08ba3dd333317b364d09a
Ahmad-B-Mahmoud <ahmadmahmoud.webdev@protonmail.com>
2021-10-26 16:34:28 +0300

    Remove Contact Form

--- components/Social.js
+++ components/Social.js
@@ -1,12 +1,13 @@
 import { Container, IconButton } from '@mui/material'
-import { FacebookTwoTone, GitHub, LinkedIn } from '@mui/icons-material'
-import Contact from './Contact'
+import { FacebookTwoTone, GitHub, LinkedIn, EmailTwoTone } from '@mui/icons-material'
 
 function Social() {
   return (
     <>
       <Container maxWidth="sm" style={{textAlign: 'center'}}>
-        <Contact />
+        <IconButton aria-label="email me" color="primary">
+          <EmailTwoTone sx={{fontSize: '2.5rem'}} />
+        </IconButton>
         <IconButton aria-label="my facebook" color="primary">
           <FacebookTwoTone sx={{fontSize: '2.5rem'}} />
         </IconButton>
